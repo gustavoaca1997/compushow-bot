c710c6086d46f7f9154ae334c89b5ea4fc7234b4
Gustavo Alejandro Castellanos <gustavoaca1997@gmail.com>
2018-06-19 20:13:10 -0400

    Se maneja el comando /start
    Se registra/actualiza el usuario al utilizar el comando /start

--- app.py
+++ app.py
@@ -10,39 +10,102 @@ import requests
 from flask import Flask, request
 from pprint import pprint
 from queue import Queue
+import psycopg2
 
-# TOKEN = os.environ.get('TOKEN')
-TOKEN = "464114645:AAHp2GvwFhbxpLrsokB2upvW5yVcUDcyGbk"
-# URL = os.environ.get('URL')
-URL = "https://compushow-bot.herokuapp.com/"
+TOKEN = os.environ.get('TOKEN')
+URL = os.environ.get('URL')
 SECRET = '/bot' + TOKEN
 UPDATE_QUEUE = Queue()
 
-HELP = 'Bot creado para el registro de pagos de los clientes del <i>Cuartico de la Sala de Computación de la USB.</i>\n'+\
-'\n\n<b>Comandos básicos:</b>\n'+\
-'<code>/start XXXXXXXX</code>\nPara registrarte como usuario del bot con cédula <code>XXXXXXXX</code>\n'+\
-'\n\n<code>/start XXXXXXXX username clave</code>\nPara registrarte como usuario administrador del bot '+\
-'(e.g, si eres JD del CEIC o Colaborador), con cédula <code>XXXXXXXX</code>, con nombre de usuario del sistema de ventas <code>username</code>'+\
-' (<code>clave</code> es la clave secreta acordada con la <i>Coordinación de Información y Tecnología</i>).\n'+\
-'\n\n<code>/comprobantes</code>\n(Sólo administrador). Para mostrar los comprobantes pendientes por registrar.\n'+\
-'\n\n<b>Cómo mandar un comprobante de pago:</b>\n'+\
-'Luego de registrarte como usuario, sólo debes <b>mandar una captura del comprobante al bot</b> y listo.'
 # Token para el cuartico
 TOKEN_CUARTICO = "chillbro2018"
 PORT = os.environ.get('PORT')
 SECRET = '/bot' + TOKEN
 UPDATE_QUEUE = Queue()
+DATABASE_URL = os.environ['DATABASE_URL']
 
+PLAYLIST_URL = 'https://open.spotify.com/user/gustavoaca1997/playlist/4Qb026FvM0ieNz4FEFgqUr'
+MSG_PLAYLIST = 'No estoy seguro de que me pides, pero seguro quieres escuchar esta maravillosa playlist: {}'\
+    .format(PLAYLIST_URL)
+
+#####################################
+########### Funciones ###############
+#####################################
+
+## Funcion que checquea si el mensaje de texto es un comando
+def is_command(text):
+    return len(text) > 0 and text[0] == '/'
+
+## Funcion que checkea si el comando es /start
+def is_start(text):
+    return is_command(text) and text[1:] == "start"
+
+## Funcion que guarda o actualiza en la base de datos el usuario con su contraseña
+def save_user(usuario, password):
+    conn = psycopg2.connect("dbname={}".format(DATABASE_URL), sslmode='require')
+    cur = conn.cursor()
+
+    # Chequeamos si ya el usuario existe
+    cur.execute('SELECT carnet FROM usuario WHERE carnet = %s;', (usuario, ))
+    usuario_guardado = cur.fetchone()
+
+    # Si no está en la base de datos, insertar
+    if not usuario_guardado:
+        cur.execute('INSERT INTO usuario (carnet, password) VALUES (%s, %s);', (usuario, password, ))
+        return 1
+
+    # Si no, actualizamos
+    else:
+        cur.execute('UPDATE usuario SET password = %s WHERE carnet = %s;', (password, usuario, ))
+        return 0
+
+#####################################
+############### Bot #################
+#####################################
 
 class ChatSesion(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(ChatSesion, self).__init__(*args, **kwargs)
 
+        # atributo que permite saber si se está esperando por el usuario y contraseña
+        self.start = False
+
     # Manejador de mensajes
     def on_chat_message(self, msg):
+        # Imprimir en consola mensaje recibido
+        print('Mensaje recibido:')
         pprint(msg)
+
+        # Obtener info basica del mensaje
         content_type, chat_type, chat_id = telepot.glance(msg)
-        bot.sendMessage(chat_id, 'hola mundo')
+        # bot.sendMessage(chat_id, '')
+
+        # Si no es un chat privado, pasar
+        if chat_type != 'private':
+            return
+
+        # Si el mensaje es texto
+        if content_type == 'text':
+            if self.start:
+                try:
+                    # Parseamos usuario y contraseña
+                    usuario, password = msg['text'].split()
+
+                    # Guardamos en la base de datos
+                    if save_user(usuario, password):
+                        bot.sendMessage(chat_id, 'Se registró exitosamente tu cuenta.')
+                    else:
+                        bot.sendMessage(chat_id, 'Se actualizó correctamente tu cuenta.')
+
+                except:
+                    bot.sendMessage(chat_id, 'Ocurrió un error leyendo el mensaje. Vuelve a intentarlo con el comando \
+                    /start')
+
+            if is_start(msg['text']):
+                bot.sendMessage(chat_id, 'Por favor envíame tu nombre de usuario (e.g carnet) y tu contraseña (e.g cédula) \
+                separadas por un espacio.')
+                self.start = True
+
 
     def on_callback_query(self, msg):
         pass
