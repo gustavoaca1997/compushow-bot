b734756b97ed1638d6732e45ef3804a685fd350e
Gustavo Alejandro Castellanos <gustavoaca1997@gmail.com>
2018-06-19 20:10:21 -0400

    Se crea un script para crear la tabla

---
+++ init.py
@@ -0,0 +1,17 @@
+import psycopg2
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL']
+if __name__ == "__main__":
+    conn = psycopg2.connect("dbname={}".format(DATABASE_URL), sslmode='require')
+    cur = conn.cursor()
+
+    # Creamos la tabla si no existe
+    cur.execute("CREATE TABLE IF NOT EXISTS usuario ( \
+        carnet CHAR(7) PRIMARY KEY, \
+        password VARCHAR(255), \
+        CHECK (carnet LIKE '[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]'));")
+
+    conn.commit()
+    cur.close()
+    conn.close()
